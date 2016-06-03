4a87f487325b2ac82d998441a11e6695b60372a0
Grant Inna <grantinna@mail.ru>
2016-06-03 16:23:48 +0300

    Подредактировала код
    Немного танцев с бубном

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function onClick() {
     var panel = document.querySelectorAll('.panel .side');
     for (var i = 0; i < panel.length; i++) {
-        panel[i].onmouseover = function () {
+        panel[i].onmouseenter = function () {
             this.style.display = 'none';
             this.parentNode.children[1].style.display = 'block';
 
-            this.parentNode.children[1].onmouseout = function () {
-                this.style.display = 'none';
-                this.parentNode.children[0].style.display = 'block';
+            var self = this.parentNode.children[1];
+            self.parentNode.onmouseenter = function () {
+                setTimeout(function() {
+                    self.style.display = 'none';
+                    self.parentNode.children[0].style.display = 'block';
+                }, 1000)
             };
         }
     }
