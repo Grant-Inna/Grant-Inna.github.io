720612a3942f2526e71a17de8948fb2b165025f9
Grant Inna <grantinna@mail.ru>
2016-06-06 19:15:59 +0300

    Полностью рабочий вариант
    Для ie отдельный .css файл, для него же .js файл

--- script.js
+++ script.js
@@ -3,17 +3,22 @@
 //Через 0.5s переворачивается обратно
 
 function onHover() {
-    if (1 - '\0') {
-        var panel = document.querySelectorAll('.panel .side');
-        for (var i = 0; i < panel.length; i++) {
-            panel[i].onmouseenter = function () {
+    if (1 - '\0') { //Отделили IE, чтобы сюда не попадали нормальный браузеры
+        //Выбираем все видимые элементы и навешиваем на них событие "onmouseenter"
+        var side = document.querySelectorAll('.side');
+        for (var i = 0; i < side.length; i++) {
+            side[i].onmouseenter = function () {
                 this.style.display = 'none';
                 this.parentNode.children[1].style.display = 'block';
 
+                //Вместо события "onmouseleave", повесим событие "onmouseenter"
+                // на родительский элемент
                 var self = this.parentNode.children[1];
                 self.parentNode.onmouseenter = function () {
+                    //Передняя панель будет возвращаться с некоторой задержкой,
+                    // дабы приблизить эффект к желаемому
                     setTimeout(function () {
-                        self.style.display = 'none';
+                        self.parentNode.children[1].style.display = 'none';
                         self.parentNode.children[0].style.display = 'block';
                     }, 500)
                 };
