efe5bb061d0a09bd18138caca0cd8fb7349297ce
Grant Inna <grantinna@mail.ru>
2016-06-08 19:45:57 +0300

    Появление работает нормально, но уход мыши с объекта часто не фиксируется

--- script.js
+++ script.js
@@ -7,27 +7,36 @@ function onHover() {
   for (var i = 0; i < side.length; i++) {
     side[i].onmouseenter = function() {
       this.style = ('transform: rotateY(180deg); z-index: 0');
-      console.log(this);
+      //console.log('Появился: ' + this.className);
       this.parentNode.children[1].style = ('transform: rotateY(0deg); z-index: 1');
+      //console.log('Исчез: ' + this.parentNode.children[1].className);
       var self = this.parentNode.children[1];
-      self.onmouseleave = function() {
-        self.style = ('z-index: 0;	transform: rotateY(-180deg)');
-        self.parentNode.children[0].style = ('z-index: 1; transform: rotateY(0deg)');
-      }
 
-/*      //Вместо события "onmouseleave", повесим событие "onmouseenter"
-      // на родительский элемент
-      .parentNode.children[1];
-      self.parentNode.onmouseenter = function () {
-        //Передняя панель будет возвращаться с некоторой задержкой,
-        // дабы приблизить эффект к желаемому
+
+      self.onmouseleave = function() {
         setTimeout(function () {
-          self.parentNode.children[1].style.display = 'none';
-          self.parentNode.children[0].style.display = 'block';
-        }, 500)
-      };*/
+
+          console.log('Исчез: ' + self.className);
+
+          self.style = ('z-index: 0;	transform: rotateY(-180deg)');
+          self.parentNode.children[0].style = ('z-index: 1; transform: rotateY(0deg)');
+          //console.log('Появился: ' + self.parentNode.children[0].className);
+
+
+          /* self.parentNode.children[1].style = ('z-index: 0;	transform: rotateY(-180deg)');
+           self.parentNode.children[0].style = ('z-index: 1; transform: rotateY(0deg)');*/
+        }, 300)
+      };
+
+      };
+
+      //Повесим дополнительно событие "onmouseenter"
+      // на родительский элемент
+ /*     self.parentNode.onmouseenter = function () {*/
+        //Передняя панель будет возвращаться с некоторой задержкой
+
     }
-  }
+
 }
 
 
