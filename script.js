7d292515dc8dfd78a32846842b02a50eb0010422
Grant Inna <grantinna@mail.ru>
2016-06-24 13:07:08 +0300

    Невостребованные файлы

--- script.js
+++
@@ -1,7 +0,0 @@
-function onHover() {
-  var panel = document.getElementsByClassName('panel');
-  panel.onmouseenter = function() {
-    var smile = document.getElementsByClassName('front');
-    smile.style = ('transform: rotateZ(180deg);');
-  }
-}
